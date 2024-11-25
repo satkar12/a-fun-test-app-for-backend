3f90f4684175feeb5a8070f45c948000187ca4a4
Satkar <gautamsatkar10@gmail.com>
2024-11-25 22:38:27 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/facebook', (req, res) => {
+    res.send('satkargautam')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
